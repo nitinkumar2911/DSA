ac6c64633ee639b18efb78954765a0fd738c93e9
NitinKumar1054 <nitin2911.in@gmail.com>
2026-01-16 12:21:03 +0530

    Time: 20 ms (96.26%) | Memory: 83 MB (68.66%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        // int[] s = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(nums);
+        return nums;
+        
+    }
+}
